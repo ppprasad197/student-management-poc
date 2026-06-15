4cfc56011af8bb99f542dc5f0c648774b7cf8935
ppprasad197 <prasadpotdar197@gmail.com>
2026-06-15 11:36:33 +0530

    Added export to excel feature front end level

--- angular/src/app/features/book/components/my-borrow-books/my-borrow-books.component.ts
+++ angular/src/app/features/book/components/my-borrow-books/my-borrow-books.component.ts
@@ -7,6 +7,10 @@ import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { filter, first } from 'rxjs';
 import { NotificationService } from '../../../../shared/services/notification.service';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+import * as  selectAuthState from '../../../../store/auth/auth.selectors';
+import { BorrowedBook } from '../../models/borrowed-book.model';
 
 @Component({
   selector: 'app-my-borrow-books',
@@ -20,14 +24,32 @@ export class MyBorrowBooksComponent {
   private store = inject(Store);
   private notification = inject(NotificationService);
 
-  borrowedBooks$ = this.store.select(
+  currentUser = this.store.select(selectAuthState.selectCurrentUser);
+
+  borrowedBooks = this.store.select(
     BookSelectors.selectMyBorrowedBooks
-  );
+  ).subscribe(data => {
+    this.books = data;
+  });
+
+  books: BorrowedBook[] = [];
+
+  username = 'user';
 
   ngOnInit() {
     this.store.dispatch(
       BookActions.loadMyBorrowedBooks()
     );
+    this.store.select(
+      BookSelectors.selectMyBorrowedBooks
+    ).subscribe(data => {
+      this.books = data;
+    });
+    this.currentUser.subscribe(user => {
+      if (user) {
+        this.username = user.userName;
+      }
+    });
   }
 
   returnBook(id: number) {
@@ -92,5 +114,43 @@ export class MyBorrowBooksComponent {
       .subscribe(error => {
         this.notification.error(error);
       });
+  }
+
+  exportToExcel() {
+
+    const excelData = this.books.map(book => ({
+      'Book Title': book.bookTitle,
+      'Author': book.author,
+      'Issue Date': book.issueDate,
+      'Due Date': book.dueDate,
+      'Student Id': book.studentId,
+      'Renew count': book.renewCount,
+      'Book Id': book.id
+    }));
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(excelData);
+    const workbook: XLSX.WorkBook = {
+      Sheets: {
+        'Borrowed Books': worksheet
+      },
+      SheetNames: ['Borrowed Books']
+    };
+
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array'
+    });
+
+    const data: Blob = new Blob(
+      [excelBuffer],
+      {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      }
+    );
+
+    saveAs(
+      data,
+      `borrowed-books-${this.username}.xlsx`
+    );
   }
 }
